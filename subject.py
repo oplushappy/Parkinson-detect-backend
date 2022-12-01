e36feb1c78ffae73d30f371e99a80cf820e3f82c
oplushappy <ken910304@gmail.com>
2022-12-01 15:57:36 +0800

    use ObjectId

--- subject.py
+++ subject.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status, UploadFile
 from fastapi.encoders import jsonable_encoder
 from typing import List
@@ -7,6 +8,7 @@ from model import Subject
 
 router = APIRouter()
 
+from bson.objectid import ObjectId
 
 
 #創建一組測試者資料
@@ -21,23 +23,24 @@ def create_subject(request: Request, subject: Subject):
     return created_subject
 
 #列出測試者所有相關資料+ 影片
-@router.get("/{id}",response_model=Subject, response_description="list all vedio about subject")
-def list_data(id: str, request: Request):
-    if (subject := request.app.db.user.find({"_id": id})) is not None:
+@router.get("/{user_id}",response_model=Subject, response_description="list all vedio about subject")
+def list_data(user_id: str, request: Request):
+    # user_id = ObjectId(user_id)
+    if (subject := request.app.db.user.find_one({"_id": ObjectId(user_id)})) is not None:
         return subject
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"subject with id {id} not found")
 
 
 #刪除影片紀錄
-@router.delete("/{id}/video/{id2}", response_description="Delete a video")
-def delete_video(id: str, id2: str, request: Request, response: Response):
-    delete_result = request.app.db.vedio.delete_one(
-        {"_id": id2,
-         "owner_id": id}
-    )
+@router.delete("/{owner_id}/video/{video_id}", response_description="Delete a video")
+def delete_video(owner_id: str, video_id: str, request: Request):
+    delete_result = request.app.db.vedio.delete_one({
+        "owner_id": owner_id,
+        "video_id": ObjectId(video_id)
+    })
     # delete_result = old_video.delete #?delete update
 
-    # if delete_result.deleted_count == 0:
+    # if delete_result.deleted_count == 0:, video_id: str, response: Response
     #     response.status_code = status.HTTP_204_NO_CONTENT
     #     return response
     #
